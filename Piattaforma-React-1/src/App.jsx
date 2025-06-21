469addf1057e84be73ce1a1508d3b8ddac5c08f3
Francis Ojiako <focathlon@gmail.com>
2025-06-21 23:35:59 +0200

    Piattaforma-react-1

---
+++ Piattaforma-React-1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Helloworld from './components/Helloworld';
+
+function App() {
+  return (
+     <>
+     <h1>Hello world</h1>
+     <Helloworld></Helloworld>
+     </>
+)};
+
+export default App
