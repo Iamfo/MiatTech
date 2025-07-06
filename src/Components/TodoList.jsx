02a6544c4842bf761499a34b3a005fd92e8df4b6
Francis Ojiako <focathlon@gmail.com>
2025-07-06 22:06:09 +0200

    focus dell'input

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,34 +1,40 @@
 import useFetch from './Components/useFetch';
 import useFilteredTodos from './useFilteredTodos';
-import { useState } from 'react';
+import { useMemo, useState, useCallback } from 'react';
 
 function TodoList() {
 
-    const {data, loading, error} = useFetch('https://jsonplaceholder.typicode.com/todos');
+    const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/todos');
     const [searchItem, setSearchItem] = useState('');
 
-    const filteredTodos = useFilteredTodos(data, searchItem);
+    const handleOnChange = useCallback((event) => {
+        setSearchItem(event.target.value);
+    }, []);
 
-    const handleOnChange =(event) => {
-        setSearchItem(event.target.value)
-    };
+    const filteredTodos = useMemo(() => {
+        if (!data) return [];
+
+        return data.filter((todo) =>
+            todo.title.toLowerCase().includes(searchItem.toLowerCase())
+        );
+    }, [data, searchItem]);
 
     if (loading) return <p>Caricamento...</p>;
     if (error) return <p>Errore: {error}</p>;
 
-    return ( 
+    return (
         <>
             <h2>lista dei Todo</h2>
 
-            <input onChange = {handleOnChange} type="text" placeholder='Cerca' />
+            <input onChange={handleOnChange} type="text" placeholder='Cerca' />
             <ul>
-                { filteredTodos && filteredTodos.map((todo) => (
-                        <li key={todo.id}> 
-                        {todo.title}
-                        {todo.completed? 'Completato' : 'Non completato' }
-                        </li>
+                {filteredTodos && filteredTodos.map((todo) => (
+                    <li key={todo.id}>
+                        {todo.title}<br />
+                        {todo.completed ? 'Completato' : 'Non completato'}
+                    </li>
                 ))}
-                       
+
             </ul>
         </>
     );
